e657d2fe322384c818d82203165d2ed2ab9efdb5
Jordi Hessels <jordi.hessels@geodan.nl>
2020-01-29 16:12:08 +0100

    home view works

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,18 +2,16 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-// werkt als je RestController ipv Controller gebruikt
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "test";
     }
 }
